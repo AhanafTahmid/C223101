0450a4aafbbbb685d17b0326d6afc58a4841b679
AhanafTahmid <ahanaftahmid55@gmail.com>
2025-05-05 01:25:57 +0600

    removed unneccesary comments

--- CSE-3640/README.md
+++ CSE-3640/README.md
@@ -3,7 +3,7 @@
 Functions of main.js
 1. Calculate and display orders
 2. Read input from fileinput.txt
-3. Write input from fileoutput.txt
+3. Write output to fileoutput.txt
 
 
 ID: C223101
--- CSE-3640/main.js
+++ CSE-3640/main.js
@@ -5,58 +5,56 @@ class CustomerOrder{
         this.orderId = orderId;
         this.customerName = customerName;
         this.orderAmount = orderAmount;
+        this.items = [this.orderId, this.customerName, this.orderAmount];
     }
 
-    //calc_display(){
-    //    let tot_am = 0, tot = 0;
-    //    let n = this.orderId.length
-    //    for(let i=0;i<n;i++){
-     //       tot+=orderId[i];
-     //       tot_am+=orderAmount[i];
-     //   }
-     //   console.log( ` Total Order = ${tot}`);
-    //    console.log( ` Total Order Amount = ${tot_am}`);
-   // }
-
+    calc_display() {
+        console.log(`Order ID: ${this.orderId}`);
+        console.log(`Customer Name: ${this.customerName}`);
+        console.log(`Order Amount: $${this.orderAmount}`);
+    }
 }
 
-
-const ob1 = new CustomerOrder(1,"Ahanaf", 4)
-const ob2 = new CustomerOrder(1,"tt", 4)
-const ob3 = new CustomerOrder(1,"ttt", 4)
-const ob4 = new CustomerOrder(1,"Ahantttttaf", 4)
+const ob1 = new CustomerOrder(1,"Ahanaf", 42)
+const ob2 = new CustomerOrder(2,"tahmid", 41)
+const ob3 = new CustomerOrder(3,"sakib", 14)
+const ob4 = new CustomerOrder(4,"khan", 34)
 const arr = [ob1, ob2, ob3, ob4];
 //for(let i  = 0; i< arr.length;i++) console.log(arr[i].customerName);
 
 //caculate and order all the display files
-function calc_display2(){
-      let tot_am = 0, tot = 0;
-      let n = arr.length
-      for(let i=0;i<n;i++){
-          tot+=arr[i].orderId;
-          tot_am+=arr[i].orderAmount;
-      }
-      console.log( ` Total Order = ${tot}`);
-      console.log( ` Total Order Amount = ${tot_am}`);
-  }
-  calc_display2();
-
+const calc_display2 = () => {
+    let tot_am = 0, tot = 0;
+    let n = arr.length
+    for(let i=0;i<n;i++){
+        tot+=arr[i].orderId;
+        tot_am+=arr[i].orderAmount;
+    }
+    console.log( ` Total Order = ${tot}`);
+    console.log( ` Total Order Amount = ${tot_am}`);
+}
 
 //writing all orders
-  fs.writeFile('fileoutput.txt',"from main.js", 'utf8', (err, arr) => {
-    if (err) {
-      console.error('Error reading file:', err);
-      return;
-    }
- });
+const writeInFile = () =>{
+    const ordersData = arr.map(x => JSON.stringify({ orderId: x.orderId, customerName: x.customerName, orderAmount: x.orderAmount })).join('\n');
+    fs.writeFileSync('fileoutput.txt', ordersData, 'utf8');
+}
 
- //READING all orders
- //Manually updated the fileinput.txt as not able to write on fileoutput.txt by using nodejs
-  fs.readFile('fileinput.txt', 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading file:', err);
-      return;
-    }
-    console.log(data);
-  });
-  
+//READING all orders
+const readfromFile = () => {
+    fs.readFile('fileinput.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            return;
+        }
+
+        const lines = data.trim().split('\n');
+        for (let i = 0; i < lines.length; i++) {
+            console.log(lines[i]);
+        }
+    });
+}
+
+// writeInFile();
+// readfromFile();
+// calc_display2();
